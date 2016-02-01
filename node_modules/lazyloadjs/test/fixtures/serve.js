07a1d898c99f58b57faef591c52410fa25b0322d
Chris Price <price.c@gmail.com>
2016-02-01 16:22:56 +0000

    Remove node_modules from source control

--- node_modules/lazyloadjs/test/fixtures/serve.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var port = process.env.ZUUL_PORT;
-var root = path.join(__dirname, '..', '..');
-
-var app = express();
-app.use('/', express.static(root));
-app.listen(port);
