fca868a41cecf0664d68a688f7d4e5f65e36746c
Chris Price <price.c@gmail.com>
2016-02-03 12:36:57 +0000

    Add t-d3fc-db container with statuses table

---
+++ db/docker-entrypoint-initdb.d/create-status-table.sql
@@ -0,0 +1,8 @@
+/* Create table using jsonb for performance */
+CREATE TABLE IF NOT EXISTS statuses ( status jsonb );
+/* Create GIN index to allow efficient querying of JSON object */
+CREATE INDEX IF NOT EXISTS statuses_index ON statuses USING GIN ( status jsonb_path_ops );
+/* Create unique index of id_str as we'll use it as our key */
+CREATE UNIQUE INDEX statuses_id_str ON statuses ( (status->>'id_str') );
+/* Create constraint to prevent null key */
+ALTER TABLE statuses ADD CONSTRAINT statuses_id_str_not_null CHECK ( ( status->>'id_str' ) IS NOT NULL );
