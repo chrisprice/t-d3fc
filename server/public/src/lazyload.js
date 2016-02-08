6e7ebbad948d70b15614a7f57f6e0f15cf6a507a
Chris Price <price.c@gmail.com>
2016-02-08 14:29:01 +0000

    Fix back button behavior and centralise iframe logic/styling

--- server/public/src/lazyload.js
+++ server/public/src/lazyload.js
@@ -1,33 +1,45 @@
-var iframes = document.querySelectorAll('iframe[data-src]');
-
-var viewportWidth   = window.innerWidth || document.documentElement.clientWidth;
-var viewportHeight  = window.innerHeight || document.documentElement.clientHeight;
+function swap(elem, attrs) {
+  var newElem = document.createElement(elem.tagName);
+  Object.keys(attrs)
+    .forEach(function(name) {
+      newElem.setAttribute(name, attrs[name]);
+    });
+  elem.parentNode.replaceChild(newElem, elem);
+}
 
 function debouncedScroll() {
-  for (var i = 0; i < iframes.length; i++) {
-    var iframe = iframes[i];
-    var bbox = iframe.getBoundingClientRect();
-    if (bbox.top <= viewportHeight && bbox.bottom >= 0) {
-      if (iframe.dataset.src) {
-        iframe.dataset.loadingSrc = iframe.src;
-        iframe.src = iframe.dataset.src;
-        delete iframe.dataset.src;
-      };
-    } else {
-      if (iframe.dataset.loadingSrc) {
-        iframe.dataset.src = iframe.src;
-        iframe.src = iframe.dataset.loadingSrc;
-        delete iframe.dataset.loadingSrc;
+  function scroll() {
+    var viewportWidth   = window.innerWidth || document.documentElement.clientWidth;
+    var viewportHeight  = window.innerHeight || document.documentElement.clientHeight;
+    var wrappers = document.querySelectorAll('.iframe-wrapper');
+    for (var i = 0; i < wrappers.length; i++) {
+      var iframe = wrappers[i].querySelector('iframe');
+      var bbox = iframe.getBoundingClientRect();
+      if (bbox.top <= viewportHeight && bbox.bottom >= 0) {
+        if (iframe.dataset.src) {
+          swap(iframe, {
+            'data-srcdoc': iframe.srcdoc,
+            'src': iframe.dataset.src,
+            'sandbox': 'allow-scripts'
+          });
+        }
+      } else {
+        if (iframe.dataset.srcdoc) {
+          swap(iframe, {
+            'data-src': iframe.src,
+            'srcdoc': iframe.dataset.srcdoc,
+            'sandbox': 'allow-scripts'
+          });
+        }
       }
     }
   }
-}
-
-addEventListener('scroll', function() {
   if (debouncedScroll.debounce) {
     clearTimeout(debouncedScroll.debounce);
   }
-  debouncedScroll.debounce = setTimeout(debouncedScroll, 100);
-});
+  debouncedScroll.debounce = setTimeout(scroll, 100);
+}
+
+addEventListener('scroll', debouncedScroll);
 
 debouncedScroll();
