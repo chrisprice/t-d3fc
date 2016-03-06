a75d0c8cec63e4f85b45be7a6afdf2c621b964f8
Chris Price <price.c@gmail.com>
2016-03-06 22:05:13 +0000

    Lazy load gifs

--- server/public/src/lazyload.js
+++ server/public/src/lazyload.js
@@ -1,35 +1,14 @@
-function swap(elem, attrs) {
-  var newElem = document.createElement(elem.tagName);
-  Object.keys(attrs)
-    .forEach(function(name) {
-      newElem.setAttribute(name, attrs[name]);
-    });
-  elem.parentNode.replaceChild(newElem, elem);
-}
-
 function debouncedScroll() {
   function scroll() {
     var viewportWidth   = window.innerWidth || document.documentElement.clientWidth;
     var viewportHeight  = window.innerHeight || document.documentElement.clientHeight;
-    var wrappers = document.querySelectorAll('.iframe-wrapper');
+    var wrappers = document.querySelectorAll('.thumbnail');
     for (var i = 0; i < wrappers.length; i++) {
-      var iframe = wrappers[i].querySelector('iframe');
-      var bbox = iframe.getBoundingClientRect();
-      if (bbox.top <= viewportHeight && bbox.bottom >= 0) {
-        if (iframe.dataset.src) {
-          swap(iframe, {
-            'data-srcdoc': iframe.srcdoc,
-            'src': iframe.dataset.src,
-            'sandbox': 'allow-scripts'
-          });
-        }
-      } else {
-        if (iframe.dataset.srcdoc) {
-          swap(iframe, {
-            'data-src': iframe.src,
-            'srcdoc': iframe.dataset.srcdoc,
-            'sandbox': 'allow-scripts'
-          });
+      var element = wrappers[i].querySelector('img');
+      if (element.dataset.src) {
+        var bbox = element.getBoundingClientRect();
+        if (bbox.top <= viewportHeight * 2 && bbox.bottom >= -viewportHeight) {
+          element.src = element.dataset.src;
         }
       }
     }
