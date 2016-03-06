3e84d4f0768cbc7615f1c24f3f3eb332d4214387
Chris Price <price.c@gmail.com>
2016-03-06 23:19:48 +0000

    Fix lazyload bug on status page

--- server/public/src/lazyload.js
+++ server/public/src/lazyload.js
@@ -2,9 +2,9 @@ function debouncedScroll() {
   function scroll() {
     var viewportWidth   = window.innerWidth || document.documentElement.clientWidth;
     var viewportHeight  = window.innerHeight || document.documentElement.clientHeight;
-    var wrappers = document.querySelectorAll('.thumbnail');
+    var wrappers = document.querySelectorAll('.thumbnail img');
     for (var i = 0; i < wrappers.length; i++) {
-      var element = wrappers[i].querySelector('img');
+      var element = wrappers[i];
       if (element.dataset.src) {
         var bbox = element.getBoundingClientRect();
         if (bbox.top <= viewportHeight * 2 && bbox.bottom >= -viewportHeight) {
